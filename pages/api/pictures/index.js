c85d69673b6c4f144c66842bc464c0136cbe27fa
Adrián Rey Lomba <adrianreylomba@gmail.com>
2022-03-11 11:17:14 +0100

    Implement upload feature

--- pages/api/pictures/index.js
+++ pages/api/pictures/index.js
@@ -1,6 +1,13 @@
 import dbConnect from "../../../lib/dbConnect";
 import Picture from "../../../models/picture";
 import handler from "../../../lib/handler";
+import upload from "../../../lib/upload";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default handler
   .get(async (req, res) => {
@@ -12,11 +19,12 @@ export default handler
       res.status(400).json({ error });
     }
   })
+  .use(upload.single("file_upload"))
   .post(async (req, res) => {
     try {
-      const { title, description, image } = req.body;
+      const { title, description } = req.body;
 
-      await Picture.create({ title, description, image });
+      await Picture.create({ title, description });
 
       res.status(201).json({ message: "Foto subida correctamente." });
     } catch (error) {
