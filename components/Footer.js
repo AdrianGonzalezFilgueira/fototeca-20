f627b9a1637ea8414b48e81a803ad912e4b56344
Adrian <adriangonzalezfilgueira@gmail.com>
2022-03-21 12:56:07 +0100

    Estilo a app.js, footer, login y upload

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,37 @@
+import { Divider, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
 export default function Footer() {
-  return <footer style={{ backgroundColor: "white" }}>SOY EL FOOTER</footer>;
+  return (
+    <footer>
+      <Divider
+        style={{
+          backgroundColor: "white",
+          margin: "0 4%",
+        }}
+        variant="middle"
+      />
+      <Box
+        gap={5}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        my={2.5}
+        color="white"
+      >
+        <Typography variant="body2">
+          &copy; {new Date().getFullYear()} Valtua, Inc.
+        </Typography>
+        <Typography variant="body2" style={{ cursor: "pointer" }}>
+          Términos de uso
+        </Typography>
+        <Typography variant="body2" style={{ cursor: "pointer" }}>
+          Aviso Legal
+        </Typography>
+        <Typography variant="body2" style={{ cursor: "pointer" }}>
+          Política de privacidad y cookies
+        </Typography>
+      </Box>
+    </footer>
+  );
 }
